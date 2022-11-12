0d20dd01e40ddbb90e8dd35d3614b03a2dc1cc02
Debojit Deb <hakrboy@gmail.com>
2022-11-12 19:37:53 +0530

    slider model changed

--- index.js
+++ index.js
@@ -9,42 +9,38 @@ app.all('/slider', (req, res) => {
     console.log("Just got a request!")
     res.send([
         {
-          "_id": "603b85ebd5b9560015caf9a8",
+          "id": "603b85ebd5b9560015caf9a8",
           "createdAt": "2021-02-28T12:00:43.980Z",
           "updatedAt": "2021-02-28T12:00:48.427Z",
-          "image": {
-            "_id": "603b85c5d5b9560015caf9a3",
-            "name": "gemma-stpjHJGqZyw-unsplash.jpg",
-            "caption": "Red Tamatarrrrrr",
-            "url": "https://res.cloudinary.com/programming-night/image/upload/v1614513603/gemma_stpj_HJ_Gq_Zyw_unsplash_9bf251892f.jpg",
-          },
-          "id": "603b85ebd5b9560015caf9a8"
+           "name": "gemma-stpjHJGqZyw-unsplash.jpg",
+           "caption": "Red Tamatarrrrrr",
+          "photoURL": "https://res.cloudinary.com/programming-night/image/upload/v1614513603/gemma_stpj_HJ_Gq_Zyw_unsplash_9bf251892f.jpg",
+            "published":"true",
+            "scheduled" : "2021-02-28T12:00:48.427Z",
+            "navigationUrl":""
         },
         {
-            "_id": "603b85ebd5b9560015caf9a8",
-            "createdAt": "2021-02-28T12:00:43.980Z",
-            "updatedAt": "2021-02-28T12:00:48.427Z",
-            "image": {
-              "_id": "603b85c5d5b9560015caf9a3",
-              "name": "gemma-stpjHJGqZyw-unsplash.jpg",
-              "caption": "Green Vegitable",
-              "url": "https://res.cloudinary.com/programming-night/image/upload/v1614513609/neonbrand_9m2_R_Zv_HS_c_U_unsplash_6a14fab32a.jpg",
-            },
-            "id": "603b85ebd5b9560015caf9a8"
+          "id": "603b85ebd5b9560015caf9a8",
+          "createdAt": "2021-02-28T12:00:43.980Z",
+          "updatedAt": "2021-02-28T12:00:48.427Z",
+           "name": "gemma-stpjsHJGqZyw-unsplash.jpg",
+            "caption": "Green Vegitable",
+          "photoURL": "https://res.cloudinary.com/programming-night/image/upload/v1614513609/neonbrand_9m2_R_Zv_HS_c_U_unsplash_6a14fab32a.jpg",
+            "published":"true",
+            "scheduled" : "2021-02-28T12:00:48.427Z",
+            "navigationUrl":""
         },
         {
-            "_id": "603b85ebd5b9560015caf9a8",
-            "createdAt": "2021-02-28T12:00:43.980Z",
-            "updatedAt": "2021-02-28T12:00:48.427Z",
-            "image": {
-              "_id": "603b85c5d5b9560015caf9a3",
-              "name": "gemma-stpjHJGqZyw-unsplash.jpg",
-              "caption": "",
-              "url": "https://res.cloudinary.com/programming-night/image/upload/v1614513607/neonbrand_Svh_XD_3k_PSTY_unsplash_11a0e33129.jpg",
-            },
-            "id": "603b85ebd5b9560015caf9a8"
-          },
-
+          "id": "603b85ebd5b9560015caf9a8",
+          "createdAt": "2021-02-28T12:00:43.980Z",
+          "updatedAt": "2021-02-28T12:00:48.427Z",
+           "name": "gemma-stpjHJGqZsayw-unsplash.jpg",
+            "caption": "Green Vegitable",
+          "photoURL": "https://res.cloudinary.com/programming-night/image/upload/v1614513607/neonbrand_Svh_XD_3k_PSTY_unsplash_11a0e33129.jpg",
+            "published":"true",
+            "scheduled" : "2021-02-28T12:00:48.427Z",
+            "navigationUrl":""
+        }
       ])
 })
 
